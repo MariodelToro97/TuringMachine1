6d4caa04aeaf17abc289bea1ef4df3a775e6792e
Mario Josue del Toro Morales <mario_chomy@outlook.com>
2018-05-26 11:58:05 -0500

    Initial commit

---
+++ src/turingmachine1/TuringMachine1.java
@@ -0,0 +1,27 @@
+/*
+* Este programa fue hecho por Mario Josue del Toro Morales
+* con Número de Control 15460644
+* Estudiante del sexto semestre de la carrera de Ingeniería en Sistemas Computacionales
+* del Instituto Tecnológico de Colima
+* Materia de Lenguajes y Autómatas I
+******Descripción************
+* Programa que usa la máquina de Turing para sacar el complemento a 1 de una cadena
+ */
+package turingmachine1;
+
+/**
+ *
+ * @author Mario Josue del Toro Morales
+ */
+public class TuringMachine1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+                
+        Principal principal = new Principal(); // Declaración de la clase que tiene el formato gráfico
+        principal.setVisible(true); //Despliegue de la ventana gráfica
+    }
+    
+}
